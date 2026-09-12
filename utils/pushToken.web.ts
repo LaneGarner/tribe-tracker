d4582f192890b75a4d14477aafdd8e555668ede9
Lane Garner <b.lane.garner@gmail.com>
2026-09-12 11:55:25 -0500

    Exclude native notifications from web bundle

---
+++ utils/pushToken.web.ts
@@ -0,0 +1,7 @@
+export async function registerForPushNotifications(): Promise<string | null> { return null; }
+export async function registerAndSavePushToken(): Promise<void> {}
+export async function savePushTokenToServer(
+  _token: string,
+  _accessToken: string,
+  _apiUrl: string
+): Promise<void> {}
